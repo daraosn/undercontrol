a5cb625c7a7aa17de55c4f0d1febf153a7b9d647
Rodolfo <bopodelvalle@gmail.com>
2016-03-10 16:51:14 +0100

    Fix small mistake

--- app/models/uc_monitor.rb
+++ app/models/uc_monitor.rb
@@ -5,5 +5,5 @@ class UcMonitor < ActiveRecord::Base
   has_many :uc_signals, through: :uc_signals_monitors
 
   validates :name, :kind, presence: true
-  validates_associated :uc_signals, :users
+  validates_associated :uc_signals, :user
 end
