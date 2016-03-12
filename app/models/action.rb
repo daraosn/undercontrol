54ab1d585170da1277ba69940ba5d164c6375ee8
Diego Araos <daraosn@gmail.com>
2016-03-12 13:22:37 +0100

    Add Action virtual class for Thing model

---
+++ app/models/action.rb
@@ -0,0 +1,34 @@
+class Action
+  def self.do json
+    action = JSON.parse json, symbolize_names: true
+    pp action
+    type = action[:type].to_sym
+    case type
+    when :email
+      email= action[:email]
+      puts "TODO: action: send email to #{email}"
+    when :http_get
+      url = action[:url]
+      puts "TODO: action: http get to #{url}"
+    when :http_post
+      url = action[:url]
+      body = action[:body]
+      puts "TODO: action: http post to #{url} with body #{body}"
+    else
+      raise "Undefined Action type: #{type}"
+    end
+  end
+
+  def self.new_email email
+    { type: :email, email: email }.to_json
+  end
+
+  def self.new_http_get url
+    { type: :http_get, url: url }.to_json
+  end
+
+  def self.new_http_get url, body
+    { type: :http_post, url: url, body: body }.to_json
+  end
+
+end
