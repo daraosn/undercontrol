4e6069028d5b239be5c67c171781a02fd1219abf
Diego Araos <daraosn@gmail.com>
2016-04-03 05:09:11 +0200

    Add backend logic to make MQTT actuators work, clean up javascripts and add secret variable for mqtt server

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def home_url
     Rails.application.secrets.home_url or 'http://undercontrol.io/'
   end
+
+  def mqtt_server
+    Rails.application.secrets.mqtt_server
+  end
 end
--- app/models/thing.rb
+++ app/models/thing.rb
@@ -5,6 +5,7 @@ class Thing < ActiveRecord::Base
   before_create :set_api_key
 
   validates_inclusion_of :alarm_threshold, :in => 0..10
+  validate :alarm_min_max
 
   def set_api_key
     require 'securerandom'
@@ -20,9 +21,15 @@ class Thing < ActiveRecord::Base
     save!
   end
 
+  def alarm_min_max
+    unless (self.alarm_min < self.alarm_max) or (self.alarm_min == 0 and self.alarm_max == 0)
+      errors.add :alarm_min, "should be lower than the alarm maximum"
+    end
+  end
+
   def check_alarm
     # do not trigger alarm if alarm's action, threshold or range are not set (null or equal to 0)
-    return if (self.alarm_action.blank?) or (not self.alarm_threshold) or (self.alarm_threshold == 0) or (self.alarm_min == 0 and self.alarm_max == 0)
+    return if (self.alarm_action.blank?) or (not self.alarm_threshold) or (self.alarm_threshold == 0) or (self.alarm_min == self.alarm_max)
     # alarm is triggered by default, unless we tell not to
     trigger_alarm = true
     # grab X last measurements
@@ -35,25 +42,39 @@ class Thing < ActiveRecord::Base
         break
       end
     end
+    # check where to trigger
+    if trigger_alarm
+      # one measurement is enough, because we already know the alarm was not in normal range
+      if measurements.last.value > self.alarm_max
+        trigger_alarm = :high
+      else
+        trigger_alarm = :low
+      end
+    end
     # finish check and decide what to do
     if trigger_alarm
-      self.trigger_alarm
+      self.trigger_alarm trigger_alarm
     else
       self.untrigger_alarm
     end
   end
 
-  def trigger_alarm
-    # do not trigger alarm if has already been triggered (i.e. to avoid spamming)
-    return if self.alarm_triggered
+  def trigger_alarm state
+    # TODO: we will let the action control multiple notifications
+    # # do not trigger alarm if has already been triggered (i.e. to avoid spamming)
+    # return if self.alarm_triggered
+    # TODO: .
     # else we change the state and execute its action
+    Action.do(self, state)
     self.update alarm_triggered: true
-    Action.do(self)
   end
 
   def untrigger_alarm
-    # do not untrigger if it's already untriggered
-    return unless self.alarm_triggered
+    # TODO: we will let the action control multiple notifications
+    # # do not untrigger if it's already untriggered
+    # return unless self.alarm_triggered
+    # TODO: .
+    Action.do(self, :normal)
     self.update alarm_triggered: false
   end
 
